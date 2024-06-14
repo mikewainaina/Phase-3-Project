4e44bd42fc9921b42a05950d06df7372addcd02d
mikewainaina <michael.wainaina@student.moringaschool.com>
2024-06-14 08:27:28 +0300

    Created a db.sqlite3

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
 
 app = FastAPI()
 
@@ -6,3 +7,11 @@ app = FastAPI()
 @app.get('/')
 def index():
     return {'message': 'Hello, World!'}
+
+register_tortoise(
+    app,
+    db_url='sqlite://db.sqlite3',
+    modules={'models': ['models']},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
